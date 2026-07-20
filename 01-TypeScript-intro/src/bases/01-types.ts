7c2c817a575f35cdb7add09b477a2d8918b9001f
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-07-20 17:00:57 -0600

    Clase 12. Tipos de datos - continuación

--- 01-TypeScript-intro/src/bases/01-types.ts
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -1,5 +1,21 @@
-export const name = 'Oscar';
+export let name: string = 'Oscar';
 export const age: number = 31;
 export const isValid: boolean = true;
 
-console.log({isValid});
+name = 'Maria';
+// name = 123;
+// name = true;
+// console.log({isValid});
+
+export const templateString = `Esto es un string
+multilinea
+que puede tener
+" dobles
+' simple
+inyectar valores ${ name }
+expresiones ${ 1 + 1 }
+números: ${ age }
+booleanos: ${ isValid }
+`
+
+console.log(templateString);
