68073e1375d88e828e9054cc1b30ac496b225ac4
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-07-20 10:45:11 -0600

    Clase 11. Tipos y bases sobre módulos

---
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -0,0 +1,5 @@
+export const name = 'Oscar';
+export const age: number = 31;
+export const isValid: boolean = true;
+
+console.log({isValid});
--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -1,7 +1,8 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { name, age } from './bases/01-types';
+import './style.css';
+import typescriptLogo from './typescript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -11,7 +12,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript para ${name} de ${age} años</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
