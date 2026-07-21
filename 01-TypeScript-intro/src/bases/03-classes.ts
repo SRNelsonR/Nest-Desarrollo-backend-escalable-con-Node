cabc78435fe3ab87bb658442b810a4b9d455a04f
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-07-21 08:47:55 -0600

    Clase 16. Getters, métodos y THIS

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -8,15 +8,33 @@ export class Pokemon {
     //     console.log('constructor llamado');
     // }
     // Es lo mismo que
+
+    get imageURL(): string {
+        return `https://pokemon.com/${ this.id }.jpg`;
+    }
+
     constructor(
         public readonly id: number,
-        public name: string
+        public name: string,
+        // public imageURL: string
     ){
         console.log('constructor llamado');
+    }
+
+    scream(){
+        console.log(`${ this.name.toUpperCase() }!!!`);
+    }
+
+    speak(){
+        console.log(`${ this.name }, ${ this.name }`);
     }
 }
 
 export const charmander = new Pokemon(4, 'Charmander');
 
-charmander.id = 10;
-charmander.name = 'Mew';
+// charmander.id = 10;
+// charmander.name = 'Mew';
+console.log(charmander.imageURL);
+
+charmander.scream();
+charmander.speak();
