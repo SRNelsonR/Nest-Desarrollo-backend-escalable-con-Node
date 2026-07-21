c78ec04f759dbc57f0d2673d187def1df389f17d
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-07-20 22:00:22 -0600

    Clase 13. Objetos e interfaces

---
+++ 01-TypeScript-intro/src/bases/02-objects.ts
@@ -0,0 +1,26 @@
+export const pokemonIds = [1, 20, 30, 34, 68];
+
+// pokemonIds.push(+'1');
+
+// console.log(pokemonIds);
+
+interface Pokemon {
+    id: number;
+    name: string;
+    age?: number;
+    // age: number | undefined; -- Atiene pero no es lo mismo
+}
+
+export const bulbasaur:Pokemon = {
+    id: 1,
+    name: 'Bulbasaur',
+    age: 2
+}
+
+export const charmander: Pokemon = {
+    id: 4,
+    name: 'Charmander',
+    age: 1
+}
+
+console.log(bulbasaur);
--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -1,4 +1,5 @@
-import { name, age } from './bases/01-types';
+// import { name, age } from './bases/01-types';
+import { bulbasaur } from './bases/02-objects.ts';
 import './style.css';
 import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
@@ -12,7 +13,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript para ${name} de ${age} años</h1>
+    <h1>Vite + TypeScript ${bulbasaur.name}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
