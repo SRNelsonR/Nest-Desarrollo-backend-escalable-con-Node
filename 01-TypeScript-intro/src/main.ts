f06d613d5d3f48071995038fb96a3db1d4aa4558
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-07-28 09:49:36 -0600

    Clase 23. Decorador de método - @Deprecated

--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -2,11 +2,12 @@
 // import { bulbasaur, pokemons } from './bases/02-objects';
 // import { charmander } from './bases/03-classes';
 // import { charmander } from './bases/04-inyection';
+// import { charmander } from './bases/05-decorators';
 import './style.css';
 import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter';
-import { charmander } from './bases/05-decorators';
+import { charmander } from './bases/06-decorators2';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
