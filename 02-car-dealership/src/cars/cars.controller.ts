c57b9728addd17487ccc78579c53edfd55a32642
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-07-31 14:48:36 -0600

    CLase 38. Pipes

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -16,8 +16,9 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById( @Param('id') id: string ){
-        console.log( { id: +id } );
-        return this.carsService.findOneById( +id );
+    getCarById( @Param('id', ParseIntPipe) id: number ){
+        console.log( { id } );
+        // throw new Error('Auxilio');
+        return this.carsService.findOneById( id );
     }
 }
