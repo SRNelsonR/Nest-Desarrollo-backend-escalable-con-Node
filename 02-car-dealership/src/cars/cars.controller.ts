c8d65e36fd5247c058ccbe7454683571d659669a
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-08-04 14:46:31 -0600

    Clase 50. Crear el nuevo carro

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -30,7 +30,8 @@ export class CarsController {
     @Post()
     // @UsePipes( ValidationPipe )
     createCar ( @Body() createCarDto: CreateCarDto ) {
-        return createCarDto;
+        // return createCarDto;
+        return this.carsService.create(createCarDto);
     }
 
     @Patch(':id')
