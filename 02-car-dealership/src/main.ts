472ac605889fbae0617b90da2ff7ca0d84e2b67e
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-08-04 14:29:08 -0600

    Clase 49. Pipes Globales - A nivel de Aplicación

--- 02-car-dealership/src/main.ts
+++ 02-car-dealership/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Solo deja la data que estoy esperando
+      whitelist: true,
+      // Informa las propiedades que no son necesarias enviar
+      forbidNonWhitelisted: true,
+    })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
