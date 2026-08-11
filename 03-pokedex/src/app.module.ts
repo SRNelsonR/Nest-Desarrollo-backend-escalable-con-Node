0de3e18af5035bf000cd5cc25e7824c3e98d9094
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-08-11 10:13:45 -0600

    Clase 71. Global Prefix

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,12 +1,14 @@
 import { join } from 'path'; // Ya viene en Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
-    })
+    }),
+    PokemonModule
   ],
 })
 export class AppModule {}
