0de3a346319c6d0c791068df7aaba1f6c557002d
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-08-12 10:11:27 -0600

    Clase 83. CustomPipes - ParseMongoIdPipe

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -93,8 +93,12 @@ export class PokemonService {
 
   async remove(id: string) {
     // return `This action removes a #${id} pokemon`;
-    const pokemon = await this.findOne( id );
-    await pokemon.deleteOne();
+    // const pokemon = await this.findOne( id );
+    // await pokemon.deleteOne();
+
+    const result = this.pokemonModel.findByIdAndDelete( id );
+
+    return result;
   }
 
   private handleException( error: any ){
