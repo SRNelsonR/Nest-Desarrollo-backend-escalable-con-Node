1c71beb0800be036a62498ef13ad735a330e35e9
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-08-17 16:48:05 -0600

    Clase 93. Tarea - Insertar Pokemons por lote y Clase 94. Resolución - Insertar Pokemons por lote

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,23 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import axios, { AxiosInstance } from 'axios';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
 
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed(){
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
-    data.results.forEach( ({ name, url }) => {
+    data.results.forEach( async ({ name, url }) => {
       // console.log({ name, url });
       const segments = url.split('/');
       const no = +segments[ segments.length - 2 ];
       console.log({ name, no });
+      try {
+        const pokemon = await this.pokemonModel.create( { name, no } );
+        return pokemon;
+      } catch ( error: any ) {
+        if( error.code === 11000 ){
+          throw new BadRequestException(`Pokemon exists in db ${ JSON.stringify( error.keyValue ) }`);
+        }
+    
+        // console.log(error);
+        throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+      }
     });
 
-    return data;
+    return 'Seed Executed!';
     // return 'Seed executed';
   }
 }
