597201d65febff9b2ad95f08b86c83a1ea5f966a
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-08-18 13:48:58 -0600

    Clase 95. Insertar multiples registros simultáneamente

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -18,25 +18,39 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed(){
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
-    data.results.forEach( async ({ name, url }) => {
+    await this.pokemonModel.deleteMany(); // Delete * from pokemons
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    // const insertPromisesArray: Promise<Pokemon>[] = [];
+    const pokemonToInsert: { name:string, no:number }[] = [];
+
+    data.results.forEach( ({ name, url }) => {
       // console.log({ name, url });
       const segments = url.split('/');
       const no = +segments[ segments.length - 2 ];
-      console.log({ name, no });
-      try {
-        const pokemon = await this.pokemonModel.create( { name, no } );
-        return pokemon;
-      } catch ( error: any ) {
-        if( error.code === 11000 ){
-          throw new BadRequestException(`Pokemon exists in db ${ JSON.stringify( error.keyValue ) }`);
-        }
+      // console.log({ name, no });
+      // try {
+      //   const pokemon = await this.pokemonModel.create( { name, no } );
+      //   return pokemon;
+      // } catch ( error: any ) {
+      //   if( error.code === 11000 ){
+      //     throw new BadRequestException(`Pokemon exists in db ${ JSON.stringify( error.keyValue ) }`);
+      //   }
     
-        // console.log(error);
-        throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
-      }
+      //   // console.log(error);
+      //   throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+      // }
+      // const pokemon = await this.pokemonModel.create( { name, no } );
+      // insertPromisesArray.push(
+      //   this.pokemonModel.create({ name, no })
+      // );
+      pokemonToInsert.push({ name, no });// [{ name:bulbasaur, no:1 }];
     });
+
+    // await Promise.all(insertPromisesArray);
+    await this.pokemonModel.insertMany( pokemonToInsert );
 
     return 'Seed Executed!';
     // return 'Seed executed';
