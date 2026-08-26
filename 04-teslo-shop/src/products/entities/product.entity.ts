1978adc9e666e9136fb3f6cca5980cda0a59610d
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-08-26 14:54:51 -0600

    Clase 127. Entidad sin relaciones

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -11,4 +11,37 @@ export class Product {
     })
     title!: string;
 
+    @Column('numeric', {
+        default: 0,
+    })
+    price!: number;
+
+    // Otra forma de hacerlo
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description!: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug!: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock!: number;
+
+    @Column('text', {
+        array: true
+    })
+    sizes!: string[];
+
+    @Column('text')
+    gender!: string;
+
+    // tags
+    // images
+
 }
