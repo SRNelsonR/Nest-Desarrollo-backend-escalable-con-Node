ea559fec44e86edfef271f49f448cb6e51fe2095
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-01 09:46:46 -0600

    Clase 149. Transacciones

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -150,11 +150,36 @@ export class ProductsService {
 
     // Create query runner
     const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    
 
     try {
-      await this.productRepository.save( product );
-      return product;
+
+      if( images ){
+        await queryRunner.manager.delete( ProductImage, { product: { id } } );
+
+        product.images = images.map(
+          image => this.productImageRepository.create( { url: image })
+        );
+      } 
+      // else {
+      //   product.images = await this.productImageRepository.findBy({ product: { id } })
+      // }
+
+      await queryRunner.manager.save( product );
+
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+      
+      // await this.productRepository.save( product );
+      // return product;
+      return this.findOnePlain( id );
     } catch (error) {
+
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+
       this.handleDBExceptions(error);
     }
   }
