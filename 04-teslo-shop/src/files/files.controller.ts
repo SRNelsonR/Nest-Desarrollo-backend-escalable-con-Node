cc925479d49059287a3fe88b1e1a8e62a7a38c76
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-02 14:34:00 -0600

    Clase 162. Servir archivos de manera controlada

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,5 +1,5 @@
 import { 
-  BadRequestException, Controller, Post, UploadedFile, 
+  BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, 
   UseInterceptors
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -9,10 +9,29 @@ import { FilesService } from './files.service';
 // import { fileFilter } from './helpers/fileFilter.helper';
 // import { fileNamer } from './helpers/fileNamer.helper';
 import { fileFilter, fileNamer } from './helpers';
+import type { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  // Se podría hacer así en caso de querer definir un tipo dinámico, tipo: product/avatar
+  // @Get(':type')
+  // Para este ejemplo se pone en duro product
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ){
+    const path = this.filesService.getStaticProductImage( imageName );
+
+    // res.status(403).json({
+    //   ok: false,
+    //   path: path
+    // });
+    // return path;
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors( FileInterceptor('file', {
@@ -31,10 +50,14 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
+    // const secureUrl = `${ file.filename }`;
+    const secureUrl = `http://localhost:3000/api/files/product/ceaa20bf-cd46-48c6-afb8-5fe661ecd2fb.png`;
+
     // console.log(file);
     return {
-      fileName: file.originalname,
-      type: file.mimetype,
+      // fileName: file.originalname,
+      // type: file.mimetype,
+      secureUrl
     };
   }
   
