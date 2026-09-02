7a23c72ff4f39a8500fc22679420b9246396938a
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-02 10:12:33 -0600

    Clase 160. Guardar imagen en filesystem

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,5 +1,9 @@
-import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { 
+  BadRequestException, Controller, Post, UploadedFile, 
+  UseInterceptors
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFilter } from './helpers/fileFilter.helper';
 
@@ -9,12 +13,16 @@ export class FilesController {
 
   @Post('product')
   @UseInterceptors( FileInterceptor('file', {
-    fileFilter: fileFilter
+    fileFilter: fileFilter,
+    // limits: { fileSize: 1000 },
+    storage: diskStorage({ 
+      destination: './static/uploads'
+    })
   }) )
   uploadProductImage(
     @UploadedFile() file: Express.Multer.File
   ){
-
+    // console.log(file);
     if( !file ){
       throw new BadRequestException('Make sure that the file is an image');
     }
--- 04-teslo-shop/src/files/helpers/fileFilter.helper.ts
+++ 04-teslo-shop/src/files/helpers/fileFilter.helper.ts
@@ -1,7 +1,7 @@
 export const fileFilter = ( req: Express.Request, file: Express.Multer.File, callback: Function ) => {
     // console.log({file});
     if( !file ) return callback( new Error('File is empty'), false );
-
+    // console.log(file);
     const fileExtension = file.mimetype.split('/')[1];
     const validExtensions = [ 'jpg', 'jpeg', 'png', 'gif' ];
 
