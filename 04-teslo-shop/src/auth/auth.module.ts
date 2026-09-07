70f94221a17099ecfe93c08974d3ea801a1eeac8
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-07 10:42:39 -0600

    Clase 175. Módulos Asíncronos

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
@@ -21,6 +22,31 @@ import { User } from './entities/user.entity';
     //     expiresIn: '2h'
     //   }
     // })
+    // Hecho de manera asincrona
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      // useFactory: () => {
+      //   console.log('JWT Secret', process.env.JWT_SECRET);
+      //   return {
+      //     secret: process.env.JWT_SECRET,
+      //     signOptions: {
+      //       expiresIn: '2h'
+      //     }
+      //   }
+      // }
+      // Utilizando Config Service
+      useFactory: ( configService: ConfigService ) => {
+        // console.log('JWT SECRET configService: ', configService.get('JWT_SECRET'));
+        // console.log('JWT Secret: ', process.env.JWT_SECRET);
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    }),
   ],
   exports: [
     TypeOrmModule
