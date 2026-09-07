ffcd157ab236df9dbd5a29824bc245dafbecd3f5
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-07 16:00:12 -0600

    Clase 177. JwtStrategy - Parte 2

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -7,11 +7,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [ AuthService, JwtStrategy ],
   imports: [
+    ConfigModule,
+    
     TypeOrmModule.forFeature([
       User
     ]),
@@ -49,7 +52,10 @@ import { User } from './entities/user.entity';
     }),
   ],
   exports: [
-    TypeOrmModule
+    TypeOrmModule,
+    JwtStrategy,
+    PassportModule,
+    JwtModule
   ]
 })
 export class AuthModule {}
