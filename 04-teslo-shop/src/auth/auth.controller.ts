71f558d536ac6f78fadbaa2bc34b64183ad87a10
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-08 09:18:30 -0600

    Clase 179. Private Route - General

--- 04-teslo-shop/src/auth/auth.controller.ts
+++ 04-teslo-shop/src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
 import { AuthService } from './auth.service';
 
 // import { CreateUserDto } from './dto/create-user.dto';
@@ -17,6 +19,16 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute() {
+    return {
+      ok: true,
+      message: 'Hola Mundo Private',
+      user: { name: 'Oscar' }
+    };
   }
 
 }
