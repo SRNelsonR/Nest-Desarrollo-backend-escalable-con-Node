66c2c47b8044a9cc7ea91c5c1ed0a11649b3fdd6
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-08 10:06:26 -0600

    Clase 181. Custom Property Decorator - GetUser

--- 04-teslo-shop/src/auth/auth.controller.ts
+++ 04-teslo-shop/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
+import { GetUser } from './decorators/get-user.decorators';
 
 // import { CreateUserDto } from './dto/create-user.dto';
 // import { LoginUserDto } from './dto/login-user.dto';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -23,11 +25,20 @@ export class AuthController {
 
   @Get('private')
   @UseGuards( AuthGuard() )
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    // @Req() request: Express.Request
+    // @GetUser(['email', 'role', 'fullName']) user: User
+    @GetUser() user: User
+  ) {
+
+    // console.log( { user: request.user } );
+    // console.log({ user });
+
     return {
       ok: true,
       message: 'Hola Mundo Private',
-      user: { name: 'Oscar' }
+      // user: { name: 'Oscar' },
+      user,
     };
   }
 
