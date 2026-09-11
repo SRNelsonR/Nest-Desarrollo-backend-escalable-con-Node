bc7db94f88b0c719b2685e201fec5531a5f7237c
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-11 10:28:21 -0600

    Clase 187. Auth en otros módulos

--- 04-teslo-shop/src/products/products.controller.ts
+++ 04-teslo-shop/src/products/products.controller.ts
@@ -4,11 +4,17 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
+
 @Controller('products')
+// Para indicar que cualquiera que use estas rutas tiene ue estar autenticado
+// @Auth()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth( ValidRoles.admin )
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -25,6 +31,7 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth( ValidRoles.admin )
   update(
     @Param('id', ParseUUIDPipe) id: string, 
     @Body() updateProductDto: UpdateProductDto) {
@@ -32,6 +39,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
+  @Auth( ValidRoles.admin )
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
--- 04-teslo-shop/src/products/products.module.ts
+++ 04-teslo-shop/src/products/products.module.ts
@@ -1,7 +1,10 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
+
+import { AuthModule } from './../auth/auth.module';
+
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 // import { Product } from './entities/product.entity';
 // import { ProductImage } from './entities/product-image.entity';
@@ -14,7 +17,8 @@ import { Product, ProductImage } from './entities';
     TypeOrmModule.forFeature([
       Product,
       ProductImage
-    ])
+    ]),
+    AuthModule,
   ],
   exports: [ 
     ProductsService,
