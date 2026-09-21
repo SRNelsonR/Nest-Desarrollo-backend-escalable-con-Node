ca7b6bdd8c423a4a4c1ce365e34db9897d60adcb
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-21 14:42:12 -0600

    Clase 200. Expandir el ApiProperty

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -11,23 +11,38 @@ import { User } from "../../auth/entities/user.entity";
 @Entity({ name: 'products' })
 export class Product {
     
-    @ApiProperty()
+    @ApiProperty({
+        example: '0f111840-46e5-4e39-93cc-598d5ed2997f',
+        description: 'Product ID',
+        uniqueItems: true,
+    })
     @PrimaryGeneratedColumn('uuid')
     id!: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'T-Shirt Teslo',
+        description: 'Product Title',
+        uniqueItems: true,
+    })
     @Column('text', {
         unique: true,
     })
     title!: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 0,
+        description: 'Product Price',
+    })
     @Column('float', {
         default: 0,
     })
     price!: number;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        description: 'Product Description',
+        default: null,
+    })
     // Otra forma de hacerlo
     @Column({
         type: 'text',
@@ -35,29 +50,46 @@ export class Product {
     })
     description!: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'T_shirt_teslo',
+        description: 'Product SLUG - for SEO',
+        uniqueItems: true,
+    })
     @Column('text', {
         unique: true
     })
     slug!: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 10,
+        description: 'Product Stock',
+        default: 0,
+    })
     @Column('int', {
         default: 0
     })
     stock!: number;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: ['M', 'XL', 'XXL'],
+        description: 'Product Sizes',
+    })
     @Column('text', {
         array: true
     })
     sizes!: string[];
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'women',
+        description: 'Product Genders',
+    })
     @Column('text')
     gender!: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'Shirt',
+        description: 'Product Tags',
+    })
     @Column('text', {
         array: true,
         default: [],
