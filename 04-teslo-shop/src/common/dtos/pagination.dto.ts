f56fed4020512e53b75dd1c3dc185a1d978fbfff
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-21 14:54:51 -0600

    Clase 201. Documentar DTOs

--- 04-teslo-shop/src/common/dtos/pagination.dto.ts
+++ 04-teslo-shop/src/common/dtos/pagination.dto.ts
@@ -1,7 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+
+    @ApiProperty({
+        default: 10,
+        description: 'How many rows do you need'
+    })
     @IsOptional()
     @IsPositive()
     // Transformar a number, por defecto es texto
@@ -9,9 +16,14 @@ export class PaginationDto {
     @Type( () => Number )
     limit?: number;
 
+    @ApiProperty({
+        default: 0,
+        description: 'How many rows do you want to skip'
+    })
     @IsOptional()
     // @IsPositive()
     @Min(0)
     @Type( () => Number )
     offset?: number;
+
 }
--- 04-teslo-shop/src/products/dto/update-product.dto.ts
+++ 04-teslo-shop/src/products/dto/update-product.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types';
+// Al tomar el PartialType de mapped-types no toma los decoradores, por eso se toma de swagger en su lugar
+import { PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
