23d2dc9604abbc2dbe5cf234dd40526646e4a45b
Oscar Nelson <oscar.lanza@agilesolutions.com>
2026-09-23 13:51:27 -0600

    Clase 210. Server - Mantener identificados los clientes

--- 04-teslo-shop/src/messages-ws/messages-ws.service.ts
+++ 04-teslo-shop/src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+interface ConnectedClients{
+    [id: string]: Socket
+}
+
+// Algo asi
+// {
+//     'qdqwfsff1': socket
+//     'qdqwfsff2': socket
+//     'qdqwfsff3': socket
+//     'qdqwfsff4': socket
+// }
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+    private connectedClients: ConnectedClients = {};
+
+    registerClient( client: Socket ){
+        this.connectedClients[client.id] = client;
+    }
+
+    removeClient( clientId: string ){
+        delete this.connectedClients[clientId];
+    }
+
+    getConnectedClients(): number{
+        return Object.keys( this.connectedClients ).length;
+    }
+
+}
